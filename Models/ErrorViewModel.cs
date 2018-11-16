6652c877d3ecfe0a309aac5a349d276b8b08128a
Xsitsu <jacoblocke21@live.com>
2018-11-15 18:25:08 -0800

    Initial Commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CST465Lab8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
